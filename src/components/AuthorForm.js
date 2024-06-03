d42ce2e4177fbf6bfa06f6184e390ec39dfdcc89
svahitha25 <svahitha25@gmail.com>
2024-06-03 15:25:00 +0530

    formik library

--- src/components/AuthorForm.js
+++ src/components/AuthorForm.js
@@ -1,7 +1,7 @@
 
 
 
-import React, { useEffect } from 'react';
+/*import React, { useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
@@ -67,4 +67,77 @@ const AuthorForm = ({ addAuthor, editAuthor, editingAuthor, setEditingAuthor }) 
   );
 };
 
+export default AuthorForm;*/
+
+
+import React, { useEffect, useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const AuthorForm = ({ addAuthor, editAuthor, editingAuthor, setEditingAuthor }) => {
+  const [initialValues, setInitialValues] = useState({
+    name: '',
+    birthDate: '',
+    biography: '',
+  });
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Required'),
+    birthDate: Yup.date().required('Required'),
+    biography: Yup.string().required('Required'),
+  });
+
+  useEffect(() => {
+    if (editingAuthor) {
+      setInitialValues(editingAuthor);
+    } else {
+      setInitialValues({
+        name: '',
+        birthDate: '',
+        biography: '',
+      });
+    }
+  }, [editingAuthor]);
+
+  return (
+    <div className="author-form">
+      <h2>{editingAuthor ? 'Edit Author' : 'Add Author'}</h2>
+      <Formik
+        initialValues={initialValues}
+        enableReinitialize={true}
+        validationSchema={validationSchema}
+        onSubmit={(values, { resetForm }) => {
+          if (editingAuthor) {
+            editAuthor({ ...editingAuthor, ...values });
+            setEditingAuthor(null);
+          } else {
+            addAuthor(values);
+          }
+          resetForm();
+        }}
+      >
+        <Form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" component="div" />
+          </div>
+          <div>
+            <label htmlFor="birthDate">Birth Date</label>
+            <Field type="date" id="birthDate" name="birthDate" />
+            <ErrorMessage name="birthDate" component="div" />
+          </div>
+          <div>
+            <label htmlFor="biography">Biography</label>
+            <Field as="textarea" id="biography" name="biography" />
+            <ErrorMessage name="biography" component="div" />
+          </div>
+          <button type="submit">{editingAuthor ? 'Update Author' : 'Add Author'}</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
 export default AuthorForm;
+
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -72,7 +72,9 @@ const BookForm = ({ addBook, editBook, editingBook, setEditingBook }) => {
 export default BookForm;
 */
 
-import React, { useEffect } from 'react';
+
+
+/*import React, { useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
@@ -141,6 +143,87 @@ const BookForm = ({ addBook, editBook, editingBook, setEditingBook }) => {
             </Form>
           );
         }}
+      </Formik>
+    </div>
+  );
+};
+
+export default BookForm;*/
+
+
+
+import React, { useEffect, useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const BookForm = ({ addBook, editBook, editingBook, setEditingBook }) => {
+  const [initialValues, setInitialValues] = useState({
+    title: '',
+    author: '',
+    isbn: '',
+    publicationDate: '',
+  });
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required('Required'),
+    author: Yup.string().required('Required'),
+    isbn: Yup.string().required('Required'),
+    publicationDate: Yup.date().required('Required'),
+  });
+
+  useEffect(() => {
+    if (editingBook) {
+      setInitialValues(editingBook);
+    } else {
+      setInitialValues({
+        title: '',
+        author: '',
+        isbn: '',
+        publicationDate: '',
+      });
+    }
+  }, [editingBook]);
+
+  return (
+    <div className="book-form">
+      <h2>{editingBook ? 'Edit Book' : 'Add Book'}</h2>
+      <Formik
+        initialValues={initialValues}
+        enableReinitialize={true}
+        validationSchema={validationSchema}
+        onSubmit={(values, { resetForm }) => {
+          if (editingBook) {
+            editBook({ ...editingBook, ...values });
+            setEditingBook(null);
+          } else {
+            addBook(values);
+          }
+          resetForm();
+        }}
+      >
+        <Form>
+          <div>
+            <label htmlFor="title">Title</label>
+            <Field type="text" id="title" name="title" />
+            <ErrorMessage name="title" component="div" />
+          </div>
+          <div>
+            <label htmlFor="author">Author</label>
+            <Field type="text" id="author" name="author" />
+            <ErrorMessage name="author" component="div" />
+          </div>
+          <div>
+            <label htmlFor="isbn">ISBN</label>
+            <Field type="text" id="isbn" name="isbn" />
+            <ErrorMessage name="isbn" component="div" />
+          </div>
+          <div>
+            <label htmlFor="publicationDate">Publication Date</label>
+            <Field type="date" id="publicationDate" name="publicationDate" />
+            <ErrorMessage name="publicationDate" component="div" />
+          </div>
+          <button type="submit">{editingBook ? 'Update Book' : 'Add Book'}</button>
+        </Form>
       </Formik>
     </div>
   );
